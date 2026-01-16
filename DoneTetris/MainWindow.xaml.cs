9388b2474f68d0a38014498760cccbfe632e5d0b
Saito-Dai <dai2525yuu@icloud.com>
2026-01-17 04:43:26 +0900

    統計表示のための変数を作成

--- DoneTetris/MainWindow.xaml.cs
+++ DoneTetris/MainWindow.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System.Text;
+﻿using System.Data;
+using System.Text;
 using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Data;
@@ -8,6 +9,9 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using System;
+using System.Linq;
+using static DoneTetris.DoneRepository;
 
 namespace DoneTetris
 {
@@ -16,6 +20,30 @@ namespace DoneTetris
     /// </summary>
     public partial class MainWindow : Window
     {
+        private readonly DoneRepository _doneRepo = new();
+        private readonly MoveRepository _moveRepo = new();
+
+        private void LoadDoneAndMino()
+        {
+            var dones = _doneRepo.GetAllDonesOrdered();
+            var moves = _moveRepo.GetAllMovesOrdered();
+
+            string today = DateTime.Now.ToString("yyyy-MM-dd");
+            int todayCount = dones.Count(d => d .DoneDate == today);
+            int totalCount = dones.Count();
+
+            int score = moves.Sum(m => m.ClearedLines);
+
+            var nextDone = _doneRepo.GetOldestUnplacedDone();
+            string nextMinoText = nextDone is null ? "-" : $"{nextDone.GrantedLengthN}";
+        }
+
+
+
+
+
+
+
         public MainWindow()
         {
             InitializeComponent();
